286ce93d992a7d6405838cc7beb60c3a9069e064
Lokesh <59980525+lokeshpara@users.noreply.github.com>
2025-03-18 21:40:22 -0500

    Add files via upload

---
+++ pages/test-animation.js
@@ -0,0 +1,13 @@
+// pages/test-animation.js
+
+import React from 'react';
+
+const TestAnimation = () => {
+  return (
+    <div>
+      <h1>This is the Test Animation page</h1>
+    </div>
+  );
+};
+
+export default TestAnimation;
